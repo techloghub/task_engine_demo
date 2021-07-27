48fea1e4cdb677ac4f7df8d1403289d6f5835eda
zeyu <zeyu@ubuntux1c>
2021-07-27 18:59:49 +0800

    add guava cache to cache the groovy clazz instance

--- src/main/java/cn/techlog/task_engine/task_resolver/GroovyTaskResolver.java
+++ src/main/java/cn/techlog/task_engine/task_resolver/GroovyTaskResolver.java
@@ -4,6 +4,8 @@ import cn.techlog.task_engine.groovy_task.GroovyTask;
 import cn.techlog.task_engine.utils.FileUtil;
 import cn.techlog.task_engine.utils.TaskUtils;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyCodeSource;
 import org.codehaus.groovy.control.CompilerConfiguration;
@@ -15,6 +17,7 @@ import java.util.Map;
 
 public class GroovyTaskResolver implements TaskResolver {
     private final static CompilerConfiguration DEFAULT_CONFIG = new CompilerConfiguration(CompilerConfiguration.DEFAULT);
+    private final static Cache<String, Class<GroovyTask>> cache = CacheBuilder.newBuilder().maximumSize(10).build();
 
     @Override
     public JSONObject resolve(JSONObject task, Map<String, Object> params) {
@@ -33,13 +36,22 @@ public class GroovyTaskResolver implements TaskResolver {
 
     public GroovyTask getGroovyTask(String scriptId) {
         try {
-            // TODO: get groovy script from db, and use a cache for it
+            Class<GroovyTask> groovyTaskClazz = cache.get(scriptId, () -> getGroovyTaskClazz(scriptId));
+            return groovyTaskClazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Class<GroovyTask> getGroovyTaskClazz(String scriptId) {
+        try {
+            // TODO: get groovy script from db or cache
             String script = FileUtil.getFileString("groovy/DemoGroovyTask.groovy");
             GroovyClassLoader loader = AccessController.doPrivileged((PrivilegedAction<GroovyClassLoader>) () ->
                     new GroovyClassLoader(GroovyTaskResolver.class.getClassLoader(), DEFAULT_CONFIG));
             GroovyCodeSource codeSource = new GroovyCodeSource(script, "GroovyTask", "/groovy");
-            Class<GroovyTask> clazz = loader.parseClass(codeSource, false);
-            return clazz.newInstance();
+            return loader.parseClass(codeSource, false);
         } catch (Exception e) {
             e.printStackTrace();
         }
